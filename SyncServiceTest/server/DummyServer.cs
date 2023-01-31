0047f3f88842a0175a3b3d2a6a85e66699ca1b90
Tidjini <tidjini.messaoudi@gmail.com>
2023-01-31 13:58:43 +0100

    Sample Dummy Server with SocketIo

--- SyncServiceTest/server/DummyServer.cs
+++ SyncServiceTest/server/DummyServer.cs
@@ -1,27 +1,102 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 using SocketIOSharp.Server;
+using SocketIOSharp.Server.Client;
 
 namespace SyncServiceTest.server
 {
+
+    static class ServerEvent
+    {
+        public static readonly string CONNECTION = "connection";
+        public static readonly string DISCONNECT = "disconnect";
+        public static readonly string ERROR = "error";
+        public static readonly string INPUT = "input";
+    }
+
+
+    internal class Logger
+    {
+        public static void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
     internal class DummyServer
     {
+        private const ushort DEFAULT_PROT = 9001;
         private readonly SocketIOServer _server;
         public ushort Port { get; set; }
-        public DummyServer(ushort port = 9001)
+
+
+
+        public DummyServer(ushort port = DEFAULT_PROT)
         {
+
+            Logger.Log($"============= DUMMY SERVER INTIALISATION : {port} =============");
             Port = port;
             _server = new SocketIOServer(new SocketIOServerOption(port));
 
         }
 
-        public DummyServer(SocketIOServer server, ushort port)
+        public DummyServer(SocketIOServer server, ushort port = DEFAULT_PROT)
         {
+
+            Logger.Log($"============= DUMMY SERVER : {port} =============");
             Port = port;
             _server = server;
         }
+
+        public void Start()
+        {
+            Logger.Log("Started");
+            _server.Start();
+            Logger.Log("Start listening to incomming connections...");
+            OnConnection();
+        }
+
+        public void Stop()
+        {
+            Logger.Log("Stopped");
+            _server.Dispose();
+            _server.Stop();
+        }
+
+
+
+
+
+        public void OnConnection()
+        {
+            _server.OnConnection((SocketIOSocket socket) =>
+            {
+                Logger.Log("Client Connected...");
+                socket.On(ServerEvent.INPUT, (JToken[] Data) =>
+                {
+
+                    foreach (JToken token in Data)
+                    {
+
+                    }
+
+                    Console.WriteLine();
+
+                    socket.Emit("echo", Data);
+                });
+
+
+                socket.On(ServerEvent.DISCONNECT, () =>
+                {
+                    Logger.Log("Client Disconnected");
+
+                });
+
+
+            });
+        }
+
+
+
+
     }
 }
